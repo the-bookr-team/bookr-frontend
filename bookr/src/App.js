18e2962822b74479db9d1d2fd82752d171875d9d
Dan Sample <danssample@gmail.com>
2019-07-10 16:42:26 -0700

    Initialized the React app

---
+++ bookr/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Bookr!</h1>
+      <p>The place for all your book rating needs</p>
+    </div>
+  );
+}
+
+export default App;
