6e6f2f20a5aa0f10a1686f92ac39d75f37a3a1f8
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-10 23:28:30 -0400

    Add footer

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Header />
       <h1>Welcome to Bookr!</h1>
       <p>The place for all your book rating needs</p>
+
+      <Footer />
     </div>
   );
 }
---
+++ bookr/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+const Footer = () => {
+  return(
+    <footer>
+      <Navigation />
+    </footer>
+  )
+}
+
+export default Footer;
