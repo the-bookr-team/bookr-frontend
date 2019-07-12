6b2458e1cf544658ab533e5e2a9241da43b088e2
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-11 23:03:56 -0400

    Specify font style

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -5,19 +5,19 @@ import Footer from './Components/Footer';
 import CollectionItem from './Components/CollectionItem';
 import CollectionItemContainer from './Components/CollectionItemContainer';
 
-
-
 function App() {
   return (
 
     <div className="App">
       <Header />
+
       <h1>Welcome to Bookr!</h1>
       <p>The place for all your book rating needs</p>
       
       <CollectionItemContainer />
 
       <Footer />
+
     </div>
   );
 }
