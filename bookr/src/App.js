2be2869cf0db940c79a77e52e0d8b5209e7af864
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-11 21:18:14 -0400

    Create CollectionItem

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import CollectionItem from './Components/CollectionItem';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Header />
       <h1>Welcome to Bookr!</h1>
       <p>The place for all your book rating needs</p>
+
+      <CollectionItem />
 
       <Footer />
     </div>
---
+++ bookr/src/Components/CollectionItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CollectionItem = () => {
+  return(
+    <div className="collection-item">
+    Collection item
+    </div>
+  )
+}
+
+export default CollectionItem;
