576a1385e2bc6e3f89744221d5b47d84ac805d92
Dan Sample <danssample@gmail.com>
2019-07-11 19:38:21 -0700

    Redesigned book carousel

--- bookr/src/Components/BookCarousel/BookCarousel.js
+++ bookr/src/Components/BookCarousel/BookCarousel.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption
-} from 'reactstrap';
+import Slider from 'react-slick';
 
 const items = [
   {
@@ -22,78 +16,106 @@ const items = [
     src: 'https://images.penguinrandomhouse.com/cover/9780345805096',
     altText: 'Slide 3',
     caption: 'Slide 3'
+  },
+  {
+    src: 'https://images.penguinrandomhouse.com/cover/9781594485756',
+    altText: 'Slide 4',
+    caption: 'Slide 4'
+  },
+  {
+    src: 'https://images.penguinrandomhouse.com/cover/9781101946626',
+    altText: 'Slide 5',
+    caption: 'Slide 5'
+  },
+  {
+    src: 'https://images.randomhouse.com/cover/9780141044668',
+    altText: 'Slide 6',
+    caption: 'Slide 6'
+  },
+  {
+    src: 'https://images.penguinrandomhouse.com/cover/9780525436140',
+    altText: 'Slide 7',
+    caption: 'Slide 7'
+  },
+  {
+    src: 'https://images.penguinrandomhouse.com/cover/9780812998955',
+    altText: 'Slide 8',
+    caption: 'Slide 8'
+  },
+  {
+    src: 'https://images.randomhouse.com/cover/9780735274259',
+    altText: 'Slide 9',
+    caption: 'Slide 9'
+  },
+  {
+    src: 'https://images.penguinrandomhouse.com/cover/9781594633409',
+    altText: 'Slide 10',
+    caption: 'Slide 10'
   }
 ];
 
-class BookCarousel extends Component {
-  state = { activeIndex: 0 };
+const NextArrow = props => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'black' }}
+      onClick={onClick}
+    />
+  );
+};
 
-  onExiting = () => {
-    this.animating = true;
-  };
+const PrevArrow = props => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'black' }}
+      onClick={onClick}
+    />
+  );
+};
 
-  onExited = () => {
-    this.animating = false;
-  };
-
-  next = () => {
-    if (this.animating) return;
-    const nextIndex =
-      this.state.activeIndex === items.length - 1
-        ? 0
-        : this.state.activeIndex + 1;
-    this.setState({ activeIndex: nextIndex });
-  };
-
-  previous = () => {
-    if (this.animating) return;
-    const nextIndex =
-      this.state.activeIndex === 0
-        ? items.length - 1
-        : this.state.activeIndex - 1;
-    this.setState({ activeIndex: nextIndex });
-  };
-
-  goToIndex = newIndex => {
-    if (this.animating) return;
-    this.setState({ activeIndex: newIndex });
-  };
-
+export default class BookCarousel extends Component {
   render() {
-    const { activeIndex } = this.state;
+    const settings = {
+      width: '100vw',
+      className: 'center',
+      arrows: true,
+      centerMode: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerPadding: '50px',
+      slidesToShow: 3,
+      speed: 500,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />
+    };
 
     const slides = items.map(item => {
       return (
-        <CarouselItem
-          onExiting={this.onExiting}
-          onExited={this.onExited}
-          key={item.src}
-        >
-          <img src={item.src} alt={item.altText} />
-        </CarouselItem>
+        <div className="solo-slide" key={item.src}>
+          <img className="slide-image" src={item.src} alt={item.altText} />
+        </div>
       );
     });
 
     return (
-      <Carousel
-        activeIndex={activeIndex}
-        next={this.next}
-        previous={this.previous}
-      >
-        {slides}
-        <CarouselControl
-          direction="prev"
-          directionText="Previous"
-          onClickHandler={this.previous}
-        />
-        <CarouselControl
-          direction="next"
-          directionText="Next"
-          onClickHandler={this.next}
-        />
-      </Carousel>
+      <div className="carousel-wrapper">
+        <Slider {...settings}>
+          {slides[0]}
+          {slides[1]}
+          {slides[2]}
+          {slides[3]}
+          {slides[4]}
+          {slides[5]}
+          {slides[6]}
+          {slides[7]}
+          {slides[8]}
+          {slides[9]}
+        </Slider>
+      </div>
     );
   }
 }
-
-export default BookCarousel;
