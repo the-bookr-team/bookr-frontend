f2f438881e731a15843e8b1ce0cb4102ad67065f
Cedricium <amaya.cedric@gmail.com>
2019-07-13 12:19:26 -0700

    Implement basic routing using `react-router`
    This commit makes it so that `/` acts as the homepage and `/book` disp-
    lays the book detail page.
    
    TODO:
    
     - once connected with redux, will need to change the `/book` route to
       use dynamic routing and accept a book id as a route parameter

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './App.css';
-import BookCarousel from './Components/BookCarousel/BookCarousel';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import CollectionItemContainer from './Components/CollectionItemContainer';
+import BookCarousel from './components/BookCarousel/BookCarousel';
+import Book from './components/Book';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CollectionItemContainer from './components/CollectionItemContainer';
+
+import { Route } from 'react-router-dom';
+
+const HomePage = () => (
+  <main>
+    <BookCarousel />
+    <CollectionItemContainer />
+  </main>
+);
+
+const BookDetailPage = () => (
+  <main>
+    <Book />
+  </main>
+);
 
 function App() {
   return (
@@ -11,8 +27,10 @@ function App() {
       <Header />
       <h1>Welcome to Bookr!</h1>
       <p>The place for all your book rating needs</p>
-      <BookCarousel />
-      <CollectionItemContainer />
+      <Route exact path="/" component={HomePage} />
+      {/* TODO - implement dynamic routing (i.e. `/book/:bookId)` */}
+      {/* Source: https://reacttraining.com/react-router/web/api/Route/route-props */}
+      <Route path="/book" component={BookDetailPage} />
       <Footer />
     </div>
   );
