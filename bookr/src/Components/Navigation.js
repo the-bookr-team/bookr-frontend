1b93d91b30d513089117af8b5586d93b557a7e1f
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-15 23:04:59 -0400

    Add login modal

--- bookr/src/Components/Navigation.js
+++ bookr/src/Components/Navigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
 import '../App.css';
+import Login from './Login';
 
 const Navigation = () => {
   return(
     <nav>
       <a href="#">Home</a>
       <a href="#">About us</a>
-      <a href="#">Sign in</a>
-      <a href="#">Get started</a>
+      <Login buttonLabel="Sign in" className="login" />
+      <Login buttonLabel="Get started" className="login" />
     </nav>
   )
 };
