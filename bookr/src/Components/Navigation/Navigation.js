9c0606809dfd2412056230d6761ff371e2ac8116
Dan Sample <danssample@gmail.com>
2019-07-19 19:12:08 -0700

    Created Link to homepage when user logs out

--- bookr/src/Components/Navigation/Navigation.js
+++ bookr/src/Components/Navigation/Navigation.js
@@ -5,30 +5,38 @@ import { Link } from 'react-router-dom';
 import '../../App.css';
 import Login from '../Login';
 
-import { logout } from '../../actions'
+import { logout } from '../../actions';
 
 const Navigation = props => {
-  return(
+  return (
     <nav>
-      <Link className="nav-item" to="/">Home</Link>
-      <Link className="nav-item" to="/about">About us</Link>
+      <Link className="nav-item" to="/">
+        Home
+      </Link>
+      <Link className="nav-item" to="/about">
+        About us
+      </Link>
       {!props.isAuthenticated ? (
         <>
           <Login buttonLabel="Sign in" className="login" />
           <Login buttonLabel="Get started" className="login" />
         </>
       ) : (
-        <Button 
-          color="link" 
-          className="nav-item"
-          onClick={props.logout}>Logout</Button>
+        <Link to={{ pathname: '/' }}>
+          <Button color="link" className="nav-item" onClick={props.logout}>
+            Logout
+          </Button>
+        </Link>
       )}
     </nav>
-  )
+  );
 };
 
 const mapStateToProps = state => ({
-  isAuthenticated: state.isAuthenticated,
+  isAuthenticated: state.isAuthenticated
 });
 
-export default connect(mapStateToProps, { logout })(Navigation);
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navigation);
